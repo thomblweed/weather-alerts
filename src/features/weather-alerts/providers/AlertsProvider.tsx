1ecda14593c05aca1b4bbcb5655b2a39a6fd6855
thom <thomblweed@gmail.com>
2025-07-06 13:54:57 +0100

    date range added initial version

--- src/features/weather-alerts/providers/AlertsProvider.tsx
+++ src/features/weather-alerts/providers/AlertsProvider.tsx
@@ -1,16 +1,44 @@
 import { useQuery } from '@tanstack/react-query';
-import { useMemo, type ReactNode } from 'react';
+import { useCallback, useMemo, useState, type ReactNode } from 'react';
 
-import { AlertsContext } from '../context/AlertsContext';
+import {
+  AlertsContextValue,
+  AlertsContextDateRangeParamsValue,
+  SetAlertsContextDateRangeParams,
+} from '../context/AlertsContext';
 import { getAlerts } from '../services/alerts.service';
 
 export const AlertsProvider = ({ children }: { children: ReactNode }) => {
+  const [params, setParams] = useState<{ start?: Date; end?: Date }>({
+    start: undefined,
+    end: undefined,
+  });
   const { data } = useQuery({
-    queryKey: ['alerts'],
-    queryFn: getAlerts,
+    queryKey: ['alerts', params],
+    queryFn: () => getAlerts({ start: params.start, end: params.end }),
   });
 
-  const value = useMemo(() => ({ alerts: data?.alerts ?? [] }), [data]);
+  const alertsValue = useMemo(() => ({ alerts: data?.alerts ?? [] }), [data]);
 
-  return <AlertsContext value={value}>{children}</AlertsContext>;
+  const paramsValue = useMemo(
+    () => ({ start: params.start, end: params.end }),
+    [params],
+  );
+
+  const handleSetParams = useCallback(
+    (params: { start?: Date; end?: Date }) => {
+      setParams((prev) => ({ ...prev, ...params }));
+    },
+    [],
+  );
+
+  return (
+    <AlertsContextValue value={alertsValue}>
+      <SetAlertsContextDateRangeParams value={handleSetParams}>
+        <AlertsContextDateRangeParamsValue value={paramsValue}>
+          {children}
+        </AlertsContextDateRangeParamsValue>
+      </SetAlertsContextDateRangeParams>
+    </AlertsContextValue>
+  );
 };
--- src/features/weather-alerts/ui/WeatherAlertsFeature.tsx
+++ src/features/weather-alerts/ui/WeatherAlertsFeature.tsx
@@ -1,4 +1,5 @@
 import { AlertsProvider } from '../providers/AlertsProvider';
+import { AlertsDateRange } from './date-range/AlertsDateRange';
 import { AlertsTable } from './table/AlertsTable';
 import { Card } from '@/ui/card';
 
@@ -6,8 +7,9 @@ export const WeatherAlertsFeature = () => {
   return (
     <div className="flex flex-col gap-4">
       <h1 className="text-2xl font-bold">Weather Alerts</h1>
-      <Card className="h-[calc(100vh-6rem)] min-h-[30rem] max-h-[70rem] overflow-y-auto p-4">
+      <Card className="h-[calc(100vh-5rem)] min-h-[30rem] max-h-[70rem] overflow-y-auto p-4">
         <AlertsProvider>
+          <AlertsDateRange />
           <AlertsTable />
         </AlertsProvider>
       </Card>
