8df02de3468af4dd4d01804d6468f50fdab349c0
thom <thomblweed@gmail.com>
2025-07-07 13:21:28 +0100

    github pages deploy test

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/weather-alerts/' : '/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
